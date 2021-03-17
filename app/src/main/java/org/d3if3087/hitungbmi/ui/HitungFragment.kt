1d2de6671d8d34fdd5c6fe39ba0d69b2605e5270
muhammad_nubli_zulfahmi <mnzulfahmi@student.telkomuniversity.ac.id>
2021-03-17 09:44:14 +0700

    3.4.Membuat navigasi sederhana

--- app/src/main/java/org/d3if3087/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if3087/hitungbmi/ui/HitungFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import org.d3if3087.hitungbmi.R
 import org.d3if3087.hitungbmi.databinding.FragmentHitungBinding
 
@@ -18,6 +19,11 @@ class HitungFragment : Fragment() {
         binding = FragmentHitungBinding.inflate(
             layoutInflater, container, false)
         binding.button.setOnClickListener { hitungBmi() }
+        binding.saranButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                R.id.action_hitungFragment_to_saranFragment
+            )
+        }
         return binding.root
     }
 
@@ -49,6 +55,7 @@ class HitungFragment : Fragment() {
         val kategori = getKategori(bmi, isMale)
         binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
         binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+        binding.saranButton.visibility = View.VISIBLE
     }
 
     private fun getKategori(bmi: Float, isMale: Boolean): String {
